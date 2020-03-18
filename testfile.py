3a9289421b17341476f8b43f3a63ce91957f4015
93lorenzo <93lorenzo@gmail.com>
2020-03-18 07:17:31 +0100

    classification dict creation

--- testfile.py
+++ testfile.py
@@ -12,12 +12,18 @@ https://datascience.stackexchange.com/questions/24339/how-is-a-splitting-point-c
 
 GINI_GAIN_KEY = "gini_gain"
 SPLIT_VALUE_KEY = "split_value"
+SONS_DICT_KEY = "sons_dict"
+MORE_EQUAL = '>='
+LESS = '<'
+FEATURE_KEY = 'feature'
+PRUNE_LEVEL = 10
 
 
 def gini_gain_calc(dataframe, feature, current_split):
     columns_list = list(dataframe.columns)
     label_column = columns_list[len(columns_list) - 1]
 
+    full_data_frame_deep_copy = dataframe.copy()
     dataframe = dataframe[[feature, label_column]]
 
     # obtain all the possible outcomes (labels)
@@ -25,12 +31,11 @@ def gini_gain_calc(dataframe, feature, current_split):
     # remove the first line in the count
     total_row = dataframe[label_column].count()
 
-    print(
-        "inside gini gain calc ***{}*** \n columns = {}, label_col = {}, \n labels = {}, total_row = {} , current_split {}".format(
-            feature, columns_list, label_column, labels_list, total_row, current_split))
+    # print(  "inside gini gain calc ***{}*** \n columns = {}, label_col = {}, \n labels = {}, total_row = {} , current_split {}".format(
+    #        feature, columns_list, label_column, labels_list, total_row, current_split))
 
     subset_left = dataframe[dataframe[feature] < current_split]
-    subset_right = dataframe[dataframe[feature] > current_split]
+    subset_right = dataframe[dataframe[feature] >= current_split]
     subsets_list = [subset_left, subset_right]
 
     label_probability = 0
@@ -40,7 +45,7 @@ def gini_gain_calc(dataframe, feature, current_split):
         # count all the row of the subset
         # remove the first line in the count
         subset_count_row = subset[feature].count()
-        print("subset count row {} feature {} ".format(subset_count_row, feature))
+        # print("subset count row {} feature {} ".format(subset_count_row, feature))
         # for each label sum -> p(label)*(1-p(label))
         # TODO
         # check in this way it goes two times -> maybe it is just because it is the same split a/b(1-a/b) = (1-a)/b(1-(1-a)/b)
@@ -50,7 +55,9 @@ def gini_gain_calc(dataframe, feature, current_split):
             count_label = subset[subset[label_column] == label][feature].count()
             label_probability = count_label / subset_count_row
             gini_subset_total += label_probability * (1 - label_probability)
-            print("count_label {} label {} label prob {} GINI SUBSET TOTAL = {}".format(count_label, label, label_probability, gini_subset_total))
+            # print("count_label {} label {} label prob {} GINI SUBSET TOTAL = {}".format(count_label, label,
+            #                                                                            label_probability,
+            #                                                                            gini_subset_total))
 
         # weighted sum of both splits
         subset_weight = subset_count_row / total_row
@@ -64,22 +71,46 @@ def gini_gain_calc(dataframe, feature, current_split):
         count_label = dataframe[dataframe[label_column] == label][feature].count()
         current_probability = count_label / total_row
         maximum_correctness += current_probability * current_probability
-        print("*** maximum_impurity *** {} , {} / {}".format(maximum_correctness, count_label, total_row))
+        # print("*** maximum_impurity *** {} , {} / {}".format(maximum_correctness, count_label, total_row))
     # the max impurity is 1 - probability to correctly assign the label (p(x)^2)
     maximum_impurity = 1 - maximum_correctness
 
-    print("maximum_impurity = {} gini_weighted_gain_total = {} ".format(maximum_impurity, gini_weighted_gain_total))
+    # print("maximum_impurity = {} gini_weighted_gain_total = {} ".format(maximum_impurity, gini_weighted_gain_total))
     # https://stats.stackexchange.com/questions/175087/basic-gini-impurity-derivation/339514#339514
     # the gain = max impurity - impurity we got
     gini_gain = maximum_impurity - gini_weighted_gain_total
-    print("gini gain is {}".format(gini_gain))
-    return gini_gain
+    # print("gini gain is {}".format(gini_gain))
+    full_data_left = full_data_frame_deep_copy[full_data_frame_deep_copy[feature] < current_split]
+    full_data_right = full_data_frame_deep_copy[full_data_frame_deep_copy[feature] >= current_split]
+    return gini_gain, {LESS: full_data_left, MORE_EQUAL: full_data_right}
 
 
-def gini_impurity(dataframe):
-    features_list = dataframe.columns[:len(dataframe.columns) - 1]
+def classification(dataframe, current_level):
+    columns_list = list(dataframe.columns)
+    label_column = columns_list[len(columns_list) - 1]
+    # obtain all the possible outcomes (labels)
+    labels_list = set(dataframe[label_column])
+
+    ##################
+    # stop condition #
+    if current_level > PRUNE_LEVEL or len(labels_list) == 1:
+        return labels_list[0]
+
+
+def training(dataframe, current_level):
+    columns_list = list(dataframe.columns)
+    label_column = columns_list[len(columns_list) - 1]
+    # obtain all the possible outcomes (labels)
+    labels_list = list(dataframe[label_column])
+    ##################
+    # STOP CONDITION #
+    print("### len set label list = {} and label list = {}".format(len(set(labels_list)), labels_list))
+    if current_level > PRUNE_LEVEL or len(set(labels_list)) == 1:
+        res = max(set(labels_list), key=labels_list.count)  # two values count are the same -> the last one in the list
+        return {'output': res}
+
+    features_list = columns_list[:len(columns_list) - 1]
     gini_dict = {}
-
     # TODO
     # think about add condition like =/!= for strings and >/< for numbers
 
@@ -88,26 +119,27 @@ def gini_impurity(dataframe):
         gini_dict.update({feature: {GINI_GAIN_KEY: 0, SPLIT_VALUE_KEY: ''}})
         # 1) order for the feature value -> ìt is only needed for the split
         # dataframe.sort_values(by=feature)
-        # 2) create the needed split - size = ( n - 1 ) unique values of the feature
-        # if we have 0 5 10, we have to check each possible split -> 2.5 7.5
         unique_set_feature = set(dataframe[feature])
-        print("not sorted: {}".format(unique_set_feature))
+        # print("not sorted: {}".format(unique_set_feature))
         unique_set_feature = sorted(unique_set_feature)
-        print("sorted: {}".format(unique_set_feature))
+        # print("sorted: {}".format(unique_set_feature))
         split_list = []
         old_value = None
 
+        # 2) create the needed split - size = ( n - 1 ) unique values of the feature
+        # if we have 0 5 10, we have to check each possible split -> 2.5 7.5
         for unique_value in unique_set_feature:
             # skip the first iteration
             if old_value:
                 current_split = (old_value + unique_value) / 2
                 split_list.append(current_split)
                 # 3) estimate the gini impurity for the current value
-                current_gain = gini_gain_calc(dataframe, feature, current_split)
+                current_gain, sons_dict = gini_gain_calc(dataframe, feature, current_split)
 
                 # update the dict with the minimum value of impurity - max of the gain
                 if current_gain > gini_dict[feature][GINI_GAIN_KEY]:
-                    gini_dict.update({feature: {GINI_GAIN_KEY: current_gain, SPLIT_VALUE_KEY: current_split}})
+                    gini_dict.update({feature: {GINI_GAIN_KEY: current_gain, SPLIT_VALUE_KEY: current_split,
+                                                SONS_DICT_KEY: sons_dict}})
             # update with the used value
             old_value = unique_value
         # 4) estimate the best impurity per split
@@ -119,23 +151,34 @@ def gini_impurity(dataframe):
     max_gain = 0
     max_split = ''
     feature = ''
+    max_sons_dict = {}
     for key, value in gini_dict.items():
         loop_dict = value
-        print(value)
-        if loop_dict['gini_gain'] > max_gain:
-            max_gain = loop_dict['gini_gain']
-            max_split = loop_dict['split_value']
+        # print(value)
+        if loop_dict[GINI_GAIN_KEY] > max_gain:
+            max_gain = loop_dict[GINI_GAIN_KEY]
+            max_split = loop_dict[SPLIT_VALUE_KEY]
+            max_sons_dict = loop_dict[SONS_DICT_KEY]
             feature = key
+
     if type(feature) != 'string':
-        print(max_gain, max_split, feature)
-        dict = {'split_value': max_split, 'feature': feature}
+        # print(max_gain, max_split, feature)
+        output_flow_dict = {SPLIT_VALUE_KEY: max_split, FEATURE_KEY: feature,
+                            LESS: training(max_sons_dict[LESS], current_level - 1),
+                            MORE_EQUAL: training(max_sons_dict[MORE_EQUAL], current_level - 1)}
+        # dict = {'split_value': max_split, 'feature': feature}
+
+    print("output_dict = {}".format(output_flow_dict))
+    return output_flow_dict
 
 
 data_path = 'Data'
 file_name = 'iris_data.csv'
 
 dataframe = pd.read_csv(os.path.join(data_path, file_name))
-training_set = dataframe.head(60)
+training_set = dataframe#.head(60)
 print(training_set)
 
-gini_impurity(training_set)  # (dataframe)
+current_level = 0
+
+training(training_set, current_level)  # (dataframe)
